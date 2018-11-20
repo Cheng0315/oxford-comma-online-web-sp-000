dbb3d9323aa612d258c33bd8f923de9bde89b90d
Cheng Vue <cvu3@ucsc.edu>
2018-11-20 10:03:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-  array.join(", ")
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    str = ""
+    array.each do |ele|
+      idx = array.index(ele)
+      last_ele_idx = array.size - 1
+      if idx == last_ele_idx
+        str += "and #{ele}"
+      else
+      str += "#{ele}, "
+    end
+  end
 end
