43f87136a71499aac8a2f38a1044bce1a9eac202
Cheng Vue <cvu3@ucsc.edu>
2018-11-20 10:08:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,11 +8,7 @@ def oxford_comma(array)
     array.each do |ele|
       idx = array.index(ele)
       last_ele_idx = array.size - 1
-      if idx == last_ele_idx
-        str += "and #{ele}"
-      else
-        str += "#{ele}, "
-      end
+      idx == last_ele_idx ? str += "and #{ele}" : str += "#{ele}, "
     end
     return str
   end
